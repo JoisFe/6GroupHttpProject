320cabc0a3642bce66f4a3633bba08e0ad5f0678
조재철 <rhsnqk@gmail.com>
2022-04-21 19:52:13 +0900

    1일차 중간과정

---
+++ ScrulProject/src/main/java/com/nhnacademy/group6Project/ResponseMessageBody.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.group6Project;
+
+import java.util.List;
+import java.util.Map;
+
+public class ResponseMessageBody {
+    private List<String> args;
+    private List<Map<String, String>> data;
+
+}
--- ScrulProject/src/main/java/com/nhnacademy/group6Project/ScurlServer.java
+++ ScrulProject/src/main/java/com/nhnacademy/group6Project/ScurlServer.java
@@ -1,4 +1,112 @@
 package com.nhnacademy.group6Project;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+//< HTTP/1.1 200 OK
+//< Date: Thu, 21 Apr 2022 08:41:37 GMT
+//< Content-Type: application/json
+//< Content-Length: 254
+//< Connection: keep-alive
+//< Server: gunicorn/19.9.0
+//< Access-Control-Allow-Origin: *
+//< Access-Control-Allow-Credentials: true
+//<
+//{
+//  "args": {},
+//  "headers": {
+//    "Accept": "*/*",
+//    "Host": "httpbin.org",
+//    "User-Agent": "curl/7.79.1",
+//    "X-Amzn-Trace-Id": "Root=1-626118c1-04208d911d9c9f45249dd141"
+//    },
+//    "origin": "112.216.11.34",
+//    "url
+
+
 public class ScurlServer {
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try {
+            serverSocket = new ServerSocket(80); //
+
+            socket = serverSocket.accept(); // 나중에 소켓 반납 ?
+
+
+            PrintStream out = new PrintStream(socket.getOutputStream());
+
+            byte[] byteArr = new byte[2048];
+
+            InputStream in = socket.getInputStream();
+            int readByteCount = in.read(byteArr);
+
+            String data = new String(byteArr, 0, readByteCount, "UTF-8");
+
+            Date currentTime = new Date();
+
+            SimpleDateFormat dateFormat =
+                new SimpleDateFormat("EEE, d MMM yyyy hh:mm:ss z", Locale.ENGLISH);
+
+            dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+            System.out.println(dateFormat.format(currentTime));
+
+            //String respondMessage[] = data.split("\r\n\r\n");
+
+            System.out.println(data);
+            String date = "Date: " + data;
+
+            String responseHeadMessage = "HTTP/1.1 200 OK\n"
+                + "Content-Type: application/json\n" + "Content-Length: 254\n" + "Connection: keep-alive\n"
+                + "Server: gunicorn/19.9.0\n" + "Access-Control-Allow-Origin: *\n" + "Access-Control-Allow-Credentials: true\n"
+                +"\n";
+            responseHeadMessage +=
+                "{\"browsers\": {\"firefox\": {\"name\": \"Firefox\",\"pref_url\": \"about:config\",\"releases\": {\"1\": {\"release_date\": \"2004-11-09\",\"status\": \"retired\",\"engine\": \"Gecko\",\"engine_version\": \"1.7\"}}}}}\n";
+
+
+            out.println(responseHeadMessage);
+//            out.println(responseHeadMessage.getBytes(StandardCharsets.UTF_8));
+            System.out.println("----------");
+            System.out.println(data);
+            String[] arr = data.split("\n");
+
+            String contentType = null;
+            String contentLength = null;
+            for (String s : arr) {
+                if (s.contains("Content-Type")) {
+                    System.out.println("xxxx");
+                    contentType = s.substring(14, s.length() - 1);
+                }
+
+                if (s.contains("Content-Length")) {
+                    contentLength = s.substring(16, s.length() - 1);
+                }
+            }
+
+            System.out.println(contentType + " " + contentLength);
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // 우리가 해야할 일
+    // 1. 서버 소켓을 만든다
+    // 2. 커맨드 창에서 우리 ip와 포트로 curl을 쏜다
+    // 3. 잘 받아오는지 출력해본다
+
+    // ip : 192.168.71.30
 }
+;
